f003fae9bf2b21eae96a903efeea252f6c4471c2
Valentin <valentin2romero@gmail.com>
2025-01-20 03:49:56 -0300

    [UPD] app enseco_report_custom - fix declared_value entregas parciales y detalle x unidad en reporte de entrega

--- enseco_report_custom/models/stock_picking.py
+++ enseco_report_custom/models/stock_picking.py
@@ -12,4 +12,8 @@ class StockPicking(models.Model):
     def _compute_declared_value(self):
         super()._compute_declared_value()
         for rec in self.filtered(lambda p: p.sale_id and p.state not in ['done', 'cancel']):
-            rec.declared_value = rec.sale_id.amount_total
+            rec.declared_value = 0
+            for move_id in rec.move_ids_without_package:
+                line_id = move_id.sale_line_id
+                price_unit_real = line_id.price_total / (line_id.product_uom_qty if (line_id.product_uom_qty > 1) else 1)
+                rec.declared_value += price_unit_real * move_id.quantity
