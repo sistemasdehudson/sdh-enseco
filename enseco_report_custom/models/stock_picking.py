6f1199ed2dd3c7395d2397c071d169be8fbe883d
Valentin <valentin2romero@gmail.com>
2025-02-07 15:23:34 -0300

    [UPD] app enseco_report_custom - fix valor declarado y columna detalle cantidad en reporte

--- enseco_report_custom/models/stock_picking.py
+++ enseco_report_custom/models/stock_picking.py
@@ -16,7 +16,7 @@ class StockPicking(models.Model):
             for move_id in rec.move_ids_without_package:
                 factor_inv = move_id.sale_line_id.product_uom.factor_inv if move_id.sale_line_id.product_uom.factor_inv else 1
                 quantity_real = move_id.quantity / factor_inv
-                price_unit_real = move_id.sale_line_id.price_total / (move_id.sale_line_id.product_uom_qty if (move_id.sale_line_id.product_uom_qty > 1) else 1)
+                price_unit_real = move_id.sale_line_id.price_subtotal / (move_id.sale_line_id.product_uom_qty if (move_id.sale_line_id.product_uom_qty > 1) else 1)
                 quantity_detail = str(quantity_real) + ' U. de ' + str(move_id.sale_line_id.product_uom.name)
                 move_id.quantity_detail = quantity_detail
                 rec.declared_value += price_unit_real * quantity_real
