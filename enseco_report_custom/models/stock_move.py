a1bd1b01acaa852e9c477a064e6b2a46a0048303
Javier Pepe <javierpepe@gmail.com>
2026-04-23 08:50:43 -0300

    enseco custom report

--- enseco_report_custom/models/stock_move.py
+++ enseco_report_custom/models/stock_move.py
@@ -12,11 +12,33 @@ class StockMove(models.Model):
     def _compute_quantity_detail(self):
         for move in self:
             factor_inv = (
-                move.sale_line_id.product_uom.factor_inv
-                if move.sale_line_id.product_uom.factor_inv
+                move.sale_line_id.product_uom_id.factor_inv
+                if move.sale_line_id.product_uom_id.factor_inv
                 else 1
             )
             quantity_real = move.quantity / factor_inv
             move.quantity_detail = (
-                str(quantity_real) + " U. de " + str(move.sale_line_id.product_uom.name)
+                str(quantity_real) + " U. de " + str(move.sale_line_id.product_uom_id.name)
+            )
+from odoo import api, fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    quantity_detail = fields.Char(
+        string="Detalle de cantidad", compute="_compute_quantity_detail", store=True
+    )
+
+    @api.depends("sale_line_id.product_uom_id", "quantity")
+    def _compute_quantity_detail(self):
+        for move in self:
+            factor_inv = (
+                move.sale_line_id.product_uom_id.factor
+                if move.sale_line_id.product_uom_id.factor
+                else 1
+            )
+            quantity_real = move.quantity / factor_inv
+            move.quantity_detail = (
+                str(quantity_real) + " U. de " + str(move.sale_line_id.product_uom_id.name)
             )
