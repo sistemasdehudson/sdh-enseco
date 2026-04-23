821517df4bbeb23742cea892bc4286ad433cb712
Javier Pepe <javierpepe@gmail.com>
2026-04-23 19:58:36 -0300

    enseco reports

--- enseco_report_custom/models/stock_picking.py
+++ enseco_report_custom/models/stock_picking.py
@@ -14,9 +14,9 @@ class StockPicking(models.Model):
         super()._compute_declared_value()
         for rec in self:
             rec.declared_value = 0
-            for move_id in rec.move_ids_without_package:
-                factor_inv = move_id.sale_line_id.product_uom_id.factor_inv if move_id.sale_line_id.product_uom_id.factor_inv else 1
-                quantity_real = move_id.quantity / factor_inv
+            for move_id in rec.move_ids:
+                factor = move_id.sale_line_id.product_uom_id.factor if move_id.sale_line_id.product_uom_id.factor else 1
+                quantity_real = move_id.quantity / factor
                 price_unit_real = move_id.sale_line_id.price_subtotal / (move_id.sale_line_id.product_uom_qty if (move_id.sale_line_id.product_uom_qty > 1) else 1)
                 quantity_detail = str(quantity_real) + ' U. de ' + str(move_id.sale_line_id.product_uom_id.name)
                 move_id.quantity_detail = quantity_detail
